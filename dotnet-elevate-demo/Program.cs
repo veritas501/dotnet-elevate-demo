92dad432c6ec7f08ddea90e34456ce449c7f28c5
veritas501 <hxzene@gmail.com>
2021-11-18 18:47:30 +0800

    添加项目文件。

---
+++ dotnet-elevate-demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace dotnet_elevate_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process currentProcess = Process.GetCurrentProcess();
+            string pid = currentProcess.Id.ToString();
+
+            Console.WriteLine(string.Format("[{0}]: 1111", pid));
+
+            // run at the very beginning (before args being used)
+            args = ElevateHelper.AutoElevate(args);
+
+            Console.WriteLine(string.Format("[{0}]: 2222", pid));
+        }
+    }
+}
